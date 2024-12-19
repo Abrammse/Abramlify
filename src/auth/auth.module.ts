21f556cd31e8899e48030fe002bbc7f003822a54
Abrammse <ebram.ashraf22322@gmail.com>
2024-12-19 14:49:46 +0200

    ت

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controller/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Userentity } from './models/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Userentity])],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,8 +2,20 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { from, Observable, switchMap } from 'rxjs';
 import { User } from '../models/user.nterface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Userentity } from '../models/user.entity';
+import { Repository } from 'typeorm';
 @Injectable()
 export class AuthService {
+
+
+       constructor(
+         @InjectRepository(Userentity)
+         private readonly userentityRepository: Repository<Userentity>,
+       ) {}
+     
+
+
   hashPassword(password: string): Observable<string> {
     return from(bcrypt.hash(password, 12));
   }
@@ -15,9 +27,15 @@ export class AuthService {
 
     return this.hashPassword(password).pipe(
         switchMap((this.hashPassword:string)=>{
-            return from(
-
-        })
-    )
+            return from( this.userentityRepository.save({}))}),
+   );
   }
 }
+  function switchMap(
+    arg0:(hashedPassword:string) =>void,):import('rxjs').OperatorFunction<string,User>{
+    throw new Error('function not implemented.');
+    }
+  
+
+
+
